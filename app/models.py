b09a83f35e4eef85aebf234ee6db9cef671e784f
Mr Karmar <chuongchung.it@gmail.com>
2023-08-04 00:50:27 +0700

    07 - complete page product

--- app/models.py
+++ app/models.py
@@ -12,10 +12,17 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     price = models.FloatField()
+    image = models.ImageField(null=True,blank=True)
     # digital = models.BooleanField(default=False,null=True,blank=False)
     def __str__(self):
         return self.name
-
+    @property
+    def ImageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 class Order(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.SET_NULL,blank=True,null=True)
     date_order = models.DateTimeField(auto_now_add=True)
